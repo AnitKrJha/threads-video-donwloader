96c2d12518da3daffd7f9d47485eacc2b6580c40
Anit Jha <anitkjha31@gmail.com>
2023-07-10 13:09:03 +0530

    fix: footer and added sitemap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div className="w-full px-4 max-w-6xl py-4 border-t-4 flex justify-between m-auto">
-      <div className="withlove">
+    <div className="w-full px-4 max-w-6xl py-4 border-t-4 flex justify-between m-auto text-sm gap-6">
+      <div className="withlove text-center">
         With ❣️ by{" "}
         <a
           href="https://linkedin.com/in/anitjha"
@@ -16,14 +16,17 @@ const Footer = (props: Props) => {
           Anit Jha
         </a>
       </div>
-      <div className="links flex gap-2 text-xs font-bold">
-        <Link href="/" className="hover:underline">
+      <div className="links flex gap-2 text-xs font-bold text-left">
+        <Link href="/" className="hover:underline text-left">
           Home
         </Link>
         <Link href="./privacy-policy" className="hover:underline">
           Privacy Policy
         </Link>
-        <Link href="./terms-and-conditions" className="hover:underline">
+        <Link
+          href="./terms-and-conditions"
+          className="hover:underline text-left"
+        >
           Terms and Conditions
         </Link>
       </div>
